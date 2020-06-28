4fa6485300dbaebdfda6b6a579b38a67001afbc2
ruixue.zhang <18729255769@163.com>
2020-06-28 10:18:26 +0800

    :sparkles: 添加gitignore文件

--- src/test/sum.test.js
+++ src/test/sum.test.js
@@ -19,12 +19,3 @@
 //         }
 //     }
 // });
-
-test('zero', () => {
-  const z = 0;
-  expect(z).not.toBeNull();
-  expect(z).toBeDefined();
-  expect(z).not.toBeUndefined();
-  expect(z).not.toBeTruthy();
-  expect(z).toBeFalsy();
-});
