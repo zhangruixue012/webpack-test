5d5dcbd5d7fe4f5f841cbc9e4b22c2425a4ed744
zhangruixue012 <18729255769@163.com>
2020-08-14 15:34:36 +0800

    :sparkles: 添加eslint配置

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "extends": [
+        "plugin:react/recommended",
+        "airbnb"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    },
+    "parser": "babel-eslint",
+};
